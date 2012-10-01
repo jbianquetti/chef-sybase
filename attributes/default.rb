1254c8f2cd957e1a01abfd0c29313aef8ab386ed
Jorge Bianquetti <jbianquetti@gmail.com>
2012-10-01 16:17:40 +0200

    added initial attributes, and install recipe

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+default['sybase']['user'] = 'sybase' # default user 
+default['sybase']['group'] = 'sybase' # default group 
+default['sybase']['user']['homedir'] = '/opt/sybase'  # default $HOME
+default['sybase']['user']['server'] = 'SYBASE' # Default ASE server name
+default['sybase']['sybase_already_installed'] = false  # By default, this is false
+default['sybase']['url']['arch'] = ""  # URL to download sybase installer
+default['sybase']['url']['arch']['checksum'] =  ""  # sha256sum of sybase installer
---
+++ recipes/install.rb
@@ -0,0 +1,127 @@
+#
+# Cookbook Name:: chef-sybase
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+
+
+include_recipe "ark"
+
+Chef::Log.info("watch out! SELINUX!!!!")
+
+# install dependencies 
+%w(libaio compat-libstdc++-33).each do |pkg|
+    package pkg
+    action :install 
+end
+
+# Create sybase user
+user node['sybase']['user'] do 
+  homedir [ node['sybase']['user']['homedir'] ] 
+  comment "Sybase Adaptive Server"
+  system true 
+end
+
+# create sybase group
+group node['sybase']['group'] do 
+  members [ node['sybase']['user'] ] 
+end
+
+# TODO:  Do somethig cool with sysctl.conf  maybe with Chef::File:Edit ! This is an horrible way !
+execute "Raising shared memory" do 
+ command "echo kernel.shmmax= >> /etc/sysctl.conf && sysctl -p"
+ not_if "grep kernel.shmmax"
+end
+
+# Download Sybase installer
+ark "sybase_installer" do
+  url "#{node['sybase']['url']['arch']}"
+  path "#{node['sybase']['user']['homedir']}"
+  owner "#{node['sybase']['user']}"
+  group "#{node['sybase']['group']}" 
+  checksum "#{node['sybase']['url']['arch']['checksum']}"
+end
+
+
+# Templates to build ASE server
+# Recommended setup is one install for user
+# Copy this recipe  and modify line template "other_template.rs" do ....
+template "server.rs" do
+  source "server.rs.erb" 
+  path    "#{node['sybase']['user']['homedir']}/sybase_installer" 
+  mode   "640"
+  action :create_if_missing
+end
+
+template "sqlloc.rs" do
+  source "sqlloc.rs.erb"
+  path   "#{node['sybase']['user']['homedir']}/sybase_installer" 
+  mode   "640"
+  action :create_if_missing
+end
+
+template "response_file.txt" do
+  source "response_file.txt.erb"
+  path   "#{node['sybase']['user']['homedir']}/sybase_installer" 
+  mode   "640"
+  action :create_if_missing
+end
+
+# fire up installer 
+# May cause problems with several installations over same user's homedir. 
+# Recommended setup is one install for user
+# Copy this recipe  and add a first line: node['sybase']['user'] = "some_other_username" 
+execute "installing sybase*" do 
+    cwd  "#{node['sybase']['user']['homedir']}/sybase_installer" 
+    command "./setup -console  -silent -file response_file.txt"
+    user     node['sybase']['user']
+    group     node['sybase']['group'] 
+    not_if { node.attribute?('sybase_already_installed') }
+end
+
+# Configuring ASE Server
+# TODO: create a LWRP 
+# ~/SYBASE.sh uses too much env keys for use with rubys' ENV, I prefer handling it with a bash resource
+bash "Configuring ASE Server" do
+  user "#{node['sybase']['user']}"
+  code <<-EOH 
+        set -e 
+        source  ~/SYBASE.sh
+        srvbuildres -r  [ [ node['sybase']['user']['homedir']"/sybase_installer/server.rs" ] 
+        sqllocres -r  [ [ node['sybase']['user']['homedir']"/sybase_installer/sqlloc.rs" ]    
+      EOH
+  creates "#{ node['sybase']['user']['homedir']}/sybase_installer/install/RUN_#{node['sybase']['user']['server']}.sh"  
+  not_if { node.attribute?('sybase_already_installed') }
+end
+
+# Set up complete?
+if File.exist? "#{ node['sybase']['user']['homedir']}/sybase_installer/install/RUN_#{node['sybase']['user']['server']}.sh" 
+   node.set['sybase_already_installed'] = true
+   node.save
+end 
+
+
+
+# Service building
+# one script per user 
+template "sybase-#{node['sybase']['user']['server']}" do
+	source "sybase.init.erb"
+        path   "/etc/init.d"
+        mode    "755"
+	variables(
+	  :server => node['sybase']['user']['server']
+	)
+end
+
+service "sybase-#{node['sybase']['user']}" do
+  supports :status => false, :restart => false, :reload => false
+  action [ :enable, :start ]
+end
+
+
+
+# TODO: Create dababase with LWRP
+
+
